fd16facc6e05bd3d6c7023e2495d7baa0b3faad0
Noah <noahmwangi@gmail.com>
2023-03-16 06:44:14 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 1;
+const num2 = 62;
+const multiply = num1*num2;
+let random=Math.floor(Math.random() * 10)
 
-// Write your code here
+const max = Math.max(10, 20, 15);
+
+const num3 = 14;
+const num4 = 5;
+const mod = 14%5;
+
